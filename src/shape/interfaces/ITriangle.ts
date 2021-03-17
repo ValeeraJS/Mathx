96f91b261eb386797b47996dc60e4f983151962b
hypnosnova <601670314@qq.com>
2021-03-17 22:37:40 +0800

    fix type error

--- src/shape/interfaces/ITriangle.ts
+++ src/shape/interfaces/ITriangle.ts
@@ -1,7 +1,5 @@
-import { IVector3Data } from "../../vector/interfaces/IVector3";
-
 export default interface ITriangle3 {
-    a: IVector3Data;
-    b: IVector3Data;
-    c: IVector3Data;
+	a: Float32Array;
+	b: Float32Array;
+	c: Float32Array;
 }
