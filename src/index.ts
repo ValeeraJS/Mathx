f0bd032c7557c4c396d3b47bbb004cba42e995f6
hypnosnova <601670314@qq.com>
2022-03-31 13:49:57 +0800

    fix easing export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./color";
 export * from "./common";
-export * from "./easing";
+export * as Easing from "./easing";
 export * from "./euler";
 export * from "./matrix";
 // export * from "./polar";
