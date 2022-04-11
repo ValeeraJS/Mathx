22c32f8eff66db36121780472d896610cc45d71b
hypnosnova <601670314@qq.com>
2022-04-11 21:29:28 +0800

    fix types export

--- build/Vector/index.d.ts
+++ build/Vector/index.d.ts
@@ -1,4 +1,4 @@
-import Vector2 from "./Vector2";
-import Vector3 from "./Vector3";
-import Vector4 from "./Vector4";
-export { Vector2, Vector3, Vector4 };
+import Vector2, { IVector2 } from "./Vector2";
+import Vector3, { IVector3 } from "./Vector3";
+import Vector4, { IVector4 } from "./Vector4";
+export { Vector2, Vector3, Vector4, IVector2, IVector3, IVector4 };
--- src/vector/index.ts
+++ src/vector/index.ts
@@ -1,5 +1,5 @@
-import Vector2 from "./Vector2";
-import Vector3 from "./Vector3";
-import Vector4 from "./Vector4";
+import Vector2, { IVector2 } from "./Vector2";
+import Vector3, { IVector3 } from "./Vector3";
+import Vector4, { IVector4 } from "./Vector4";
 
-export { Vector2, Vector3, Vector4 };
+export { Vector2, Vector3, Vector4, IVector2, IVector3, IVector4 };
