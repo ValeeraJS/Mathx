987969ac2e42dfeaafe58717669cddee7238e256
hypnosnova <601670314@qq.com>
2022-04-11 13:42:48 +0800

    fix types

--- build/shape/Sphere.d.ts
+++ build/shape/Sphere.d.ts
@@ -1,10 +1,10 @@
-import { IVector3 } from "../vector/interfaces/IVector3";
+import { IVector3 } from "./../vector/Vector3";
 import ICube from "./interfaces/ICube";
 import ISphere from "./interfaces/ISphere";
 export default class Sphere implements ISphere {
     static boundingBox: (a: ISphere, out?: ICube) => ICube;
-    static containsPoint: (a: ISphere, b: IVector3Data) => boolean;
-    static distanceToPoint: (a: ISphere, b: IVector3Data) => number;
+    static containsPoint: (a: ISphere, b: IVector3 | number[] | Float32Array) => boolean;
+    static distanceToPoint: (a: ISphere, b: IVector3 | number[] | Float32Array) => number;
     static equals: (a: ISphere, b: ISphere) => boolean;
     static intersectsSphere: (a: ISphere, b: ISphere) => boolean;
     position: IVector3;
