cb1033cfa491cf4974d191d56e457c0b619c4761
HypnosNova <601670314@qq.com>
2023-01-19 23:26:21 -0800

    Update gird3.ts

--- src/grid/gird3.ts
+++ src/grid/gird3.ts
@@ -1,90 +1,30 @@
-import Vector3, { Vector3Like } from "../vector/Vector3";
-import Sphere from "../shape/Sphere";
-import ICube from "../shape/interfaces/ICube";
-import ISphere from "../shape/interfaces/ISphere";
-import Cube from "../shape/Cube";
-
-export default class Grid3 {
-	public data: Vector3Like[] = []; // 格子索引
-	public gridSize: Vector3Like; // 格子尺寸，默认1*1*1
-	public min: Vector3Like; // 格子位置，默认0,0,0
-	public size: Vector3Like; // 格子宽高深的最大数量
-	public max: Vector3Like;
-
-	public constructor(
-		size: Vector3Like = new Vector3(Infinity, Infinity, Infinity),
-		gridSize: Vector3Like = new Vector3(1, 1, 1),
-		min: Vector3Like = new Vector3()
-	) {
-		this.size = size;
-		this.gridSize = gridSize;
-		this.min = min;
-		this.max = Vector3.add(Vector3.multiply(this.gridSize, this.size), this.min);
-	}
+import Vector3, { IVector3, Vector3Like } from "../vector/Vector3";
+import ICube from "./interfaces/ICube";
+import ISphere from "./interfaces/ISphere";
+import ITriangle3 from "./interfaces/ITriangle3";
+export default class Cube implements ICube {
+    static clampPoint: (a: ICube, point: Vector3Like, out?: Vector3) => Vector3;
+    static containsPoint: (a: ICube, b: Vector3Like) => boolean;
+    static containsCube: (a: ICube, b: ICube) => boolean;
+    static depth: (a: ICube) => number;
+    static equals: (a: ICube, b: ICube) => boolean;
+    static getCenter: (a: ICube, out?: Vector3) => Vector3;
+    static getSize: (a: ICube, out?: Vector3) => Vector3;
+    static height: (a: ICube) => number;
+    static intersect: (a: ICube, b: ICube, out?: Cube) => Cube;
+    static intersectsBox: (a: ICube, b: ICube) => boolean;
+    static intersectsSphere: (a: ICube, b: ISphere) => boolean;
+    static intersectsTriangle: (a: ICube, b: ITriangle3) => boolean;
+    static isEmpty: (a: ICube) => boolean;
+    static round: (a: ICube, out?: Cube) => Cube;
+    static size: (a: ICube, out?: Vector3) => Vector3;
+    static stretch: (a: ICube, b: Float32Array | number[] | IVector3, c: Float32Array | number[] | IVector3, out?: Cube) => Cube;
+    static surfaceArea: (a: ICube) => number;
+    static translate: (a: ICube, b: Float32Array | number[] | IVector3, out?: Cube) => Cube;
+    static union: (a: ICube, b: ICube, out?: Cube) => Cube;
+    static volume: (a: ICube) => number;
+    static width: (a: ICube) => number;
+    min: Vector3;
+    max: Vector3;
+    constructor(a?: Vector3Like, b?: Vector3Like);
 }
-
-let tmpCude: Cube;
-let min3: Vector3 = new Vector3();
-let max3: Vector3 = new Vector3();
-let x: number;
-let y: number;
-let z: number;
-let rr: number;
-let i: number;
-let j: number;
-let k: number;
-let sx: number;
-let sy: number;
-let sz: number;
-
-// 将方块信息转化为格子数组，方块与格子重合部分才会记录信息
-export const dataFromSolidCube = (a: ICube, out: Grid3 = new Grid3()): Grid3 => {
-	Vector3.max(a.min, out.min, min3); // 起始遍历位置
-	Vector3.min(a.max, out.max, max3);
-
-	x = out.gridSize[0];
-	y = out.gridSize[1];
-	z = out.gridSize[2];
-
-	sx = Math.round((min3[0] - out.min[0]) / out.gridSize[0]);
-	sy = Math.round((min3[1] - out.min[1]) / out.gridSize[1]);
-	sy = Math.round((min3[2] - out.min[2]) / out.gridSize[2]);
-
-	for (i = min3[0]; i <= max3[0]; i += x, sx++) {
-		for (j = min3[1]; j <= max3[1]; j += y, sy++) {
-			for (k = min3[2]; k <= max3[2]; k += z, sz++) {
-				out.data.push(new Float32Array([sx, sy, sz]));
-			}
-		}
-	}
-
-	return out;
-};
-
-// 将球信息转化为格子数组，方块与格子重合部分才会记录信息
-export const dataFromSolidSphere = (a: ISphere, out: Grid3 = new Grid3()): Grid3 => {
-	Sphere.boundingBox(a, tmpCude);
-	Vector3.max(tmpCude.min, out.min, min3); // 起始遍历位置
-	Vector3.min(tmpCude.max, out.max, max3);
-
-	x = out.gridSize[0];
-	y = out.gridSize[1];
-	z = out.gridSize[2];
-	rr = a.radius * a.radius;
-
-	sx = Math.round((min3[0] - out.min[0]) / out.gridSize[0]);
-	sy = Math.round((min3[1] - out.min[1]) / out.gridSize[1]);
-	sy = Math.round((min3[2] - out.min[2]) / out.gridSize[2]);
-
-	for (i = min3[0]; i <= max3[0]; i += x, sx++) {
-		for (j = min3[1]; j <= max3[1]; j += y, sy++) {
-			for (k = min3[2]; k <= max3[2]; k += z, sz++) {
-				if (rr >= i * i + j * j + k * k) {
-					out.data.push(new Float32Array([sx, sy, sz]));
-				}
-			}
-		}
-	}
-
-	return out;
-};
