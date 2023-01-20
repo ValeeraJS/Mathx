a6cb165c7a4e6eab5f0965545b4f59a8b8bd1194
HypnosNova <601670314@qq.com>
2023-01-20 02:00:40 -0800

    Update Ray3.ts

--- src/ray/Ray3.ts
+++ src/ray/Ray3.ts
@@ -1,6 +1,7 @@
 import Vector3, { Vector3Like } from "../vector/Vector3";
 import IRay3 from "./interfaces/IRay3";
 import ISphere from "../shape/interfaces/ISphere";
+import { Plane3 } from "../shape/Plane3";
 
 let dis: number, r2: number, d2: number;
 const v = new Vector3();
@@ -9,6 +10,22 @@ export default class Ray3 implements IRay3 {
 	public static at = (a: IRay3, b: number, out: Vector3 = new Vector3()): Vector3 => {
 		return Vector3.multiplyScalar(a.direction, b, out);
 	};
+
+	public static distanceToPlane = (ray: Ray3, plane: Plane3) => {
+		const denominator = Vector3.dot(plane.normal, ray.direction);
+
+		if (denominator === 0) {
+			// line is coplanar, return origin
+			if (plane.distanceToPoint(ray.position) === 0) {
+				return 0;
+			}
+			return null;
+		}
+
+		const t = - (Vector3.dot(ray.position, plane.normal) + plane.distance) / denominator;
+
+		return t >= 0 ? t : null;
+	}
 
 	public static distanceToPoint = (a: IRay3, point: Vector3Like): number => {
 		return Math.sqrt(Ray3.distanceSqToPoint(a, point));
@@ -38,7 +55,17 @@ export default class Ray3 implements IRay3 {
 		return out;
 	};
 
-	public static intersectSphere = (
+	public static intersectPlanePoint = (ray: Ray3, plane: Plane3, out: Vector3 = new Vector3()): Vector3 | null => {
+		const t = Ray3.distanceToPlane(ray, plane);
+
+		if (t === null) {
+			return null;
+		}
+
+		return Ray3.at(ray, t, out);
+	}
+
+	public static intersectSpherePoint = (
 		ray: IRay3,
 		sphere: ISphere,
 		target: Vector3
@@ -63,15 +90,38 @@ export default class Ray3 implements IRay3 {
 		return Ray3.at(ray, t0, target);
 	};
 
-	public static intersectsSphere = (ray: IRay3, sphere: ISphere): boolean => {
+	public static isIntersectSphere = (ray: IRay3, sphere: ISphere): boolean => {
 		return Ray3.distanceSqToPoint(ray, sphere.position) <= sphere.radius * sphere.radius;
 	};
+
+	public static intersectsPlane = (ray: Ray3, plane: Plane3): boolean => {
+		const distToPoint = plane.distanceToPoint(ray.position);
+
+		if (distToPoint === 0) {
+			return true;
+		}
+
+		const denominator = Vector3.dot(plane.normal, ray.direction);
+
+		if (denominator * distToPoint < 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static recast = (ray: IRay3, distance: number, out: Ray3 = new Ray3()) => {
+		v.set(Ray3.at(ray, distance));
+		out.direction.set(v);
+
+		return out;
+	}
 
 	public position: Vector3 = new Vector3();
 	public direction: Vector3 = new Vector3();
 	public constructor(
 		position: Vector3Like = Vector3.VECTOR3_ZERO,
-		direction: Vector3Like =  Vector3.VECTOR3_BACK
+		direction: Vector3Like = Vector3.VECTOR3_BACK
 	) {
 		this.position.set(position)
 		Vector3.normalize(direction, this.direction);
