23cbea6aef7a83ba6980244256f9e42c1fe53ad3
大聪明 <601670314@qq.com>
2023-07-10 02:23:25 -0700

    Update Plane3.ts

--- src/shape/Plane3.ts
+++ src/shape/Plane3.ts
@@ -6,79 +6,79 @@ const v2 = new Vector3();
 const v3 = new Vector3();
 
 export class Plane3 {
-    static normalize(p: Plane3, out: Plane3 = new Plane3): Plane3 {
-        const normal = p.normal;
+	static normalize(p: Plane3, out: Plane3 = new Plane3): Plane3 {
+		const normal = p.normal;
 
-        const factor = 1.0 / Vector3.norm(normal);
-        Vector3.multiplyScalar(normal, factor, out.normal);
-        out.distance = p.distance * factor;
+		const factor = 1.0 / Vector3.norm(normal);
+		Vector3.multiplyScalar(normal, factor, out.normal);
+		out.distance = p.distance * factor;
 
-        return out;
-    }
-    public normal: Vector3 = new Vector3();
-    public distance: number;
-    constructor(normal: Vector3 = Vector3.VECTOR3_TOP, distance: number = 0) {
-        this.normal.set(normal);
-        this.distance = distance;
-    }
+		return out;
+	}
+	public normal: Vector3 = new Vector3();
+	public distance: number;
+	constructor(normal: Vector3 = Vector3.VECTOR3_TOP, distance: number = 0) {
+		this.normal.set(normal);
+		this.distance = distance;
+	}
 
-    distanceToPoint(point: Vector3Like) {
-        return Vector3.dot(this.normal, point) + this.distance;
-    }
+	distanceToPoint(point: Vector3Like): number {
+		return Vector3.dot(this.normal, point) + this.distance;
+	}
 
-    distanceToSphere(sphere: Sphere) {
-        return this.distanceToPoint(sphere.position) - sphere.radius;
-    }
+	distanceToSphere(sphere: Sphere): number {
+		return this.distanceToPoint(sphere.position) - sphere.radius;
+	}
 
-    from(p: Plane3) {
-        this.distance = p.distance;
-        this.normal.set(p.normal);
-    }
+	from(p: Plane3) {
+		this.distance = p.distance;
+		this.normal.set(p.normal);
+	}
 
-    fromCoplanarPoints(a: Vector3Like, b: Vector3Like, c: Vector3Like): this {
-        Vector3.minus(b, a, v1);
-        Vector3.minus(c, a, v2);
-        Vector3.cross(v1, v2, v3);
-        Vector3.normalize(v3, v3);
+	fromCoplanarPoints(a: Vector3Like, b: Vector3Like, c: Vector3Like): this {
+		Vector3.minus(b, a, v1);
+		Vector3.minus(c, a, v2);
+		Vector3.cross(v1, v2, v3);
+		Vector3.normalize(v3, v3);
 
-        // TODO check zero vec
+		// TODO check zero vec
 
-        return this.formCoplanarPointAndNormal(a, v3);
-    }
+		return this.formCoplanarPointAndNormal(a, v3);
+	}
 
-    formCoplanarPointAndNormal(point: Vector3Like, normal: Vector3Like): this {
-        this.normal.set(normal);
-        this.distance = - Vector3.dot(point, normal);
+	formCoplanarPointAndNormal(point: Vector3Like, normal: Vector3Like): this {
+		this.normal.set(normal);
+		this.distance = - Vector3.dot(point, normal);
 
-        return this;
-    }
+		return this;
+	}
 
-    negate(): this {
+	negate(): this {
 
-        this.distance *= - 1;
-        Vector3.negate(this.normal, this.normal);
+		this.distance *= - 1;
+		Vector3.negate(this.normal, this.normal);
 
-        return this;
+		return this;
 
-    }
+	}
 
-    normalize(): this {
-        Plane3.normalize(this, this);
+	normalize(): this {
+		Plane3.normalize(this, this);
 
-        return this;
-    }
+		return this;
+	}
 
-    projectPoint(point: Vector3Like, out: Vector3 = new Vector3()): Vector3 {
-        out.set(this.normal);
-        Vector3.multiplyScalar(out, - this.distanceToPoint(point), out);
+	projectPoint<T extends Vector3Like = Vector3>(point: Vector3Like, out: T = new Vector3() as T): T {
+		Vector3.fromArray(this.normal, 0, out);
+		Vector3.multiplyScalar(out, - this.distanceToPoint(point), out);
 
-        return Vector3.add(out, point, out);
-    }
+		return Vector3.add(out, point, out);
+	}
 
-    set(normal: Vector3Like, distance = this.distance): this {
-        this.normal.set(normal);
-        this.distance = distance;
+	set(normal: Vector3Like, distance = this.distance): this {
+		this.normal.set(normal);
+		this.distance = distance;
 
-        return this;
-    }
+		return this;
+	}
 }
