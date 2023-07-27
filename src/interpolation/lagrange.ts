647129a688b747277b38386b29bca4a577f7d942
大聪明 <601670314@qq.com>
2023-07-27 07:03:13 -0700

    Create lagrange.ts

---
+++ src/interpolation/lagrange.ts
@@ -0,0 +1,22 @@
+import { Vector2Like } from "../vector/Vector2";
+
+export const generateLagrange = (points: Vector2Like[]) => {
+	var n = points.length - 1;
+
+	const p = function (i: number, j: number, x: number) {
+		if (i === j) {
+			return points[i][1];
+		}
+
+		return ((points[j][0] - x) * p(i, j - 1, x) +
+			(x - points[i][0]) * p(i + 1, j, x)) /
+			(points[j][0] - points[i][0]);
+	};
+
+	return function (x: number) {
+		if (points.length === 0) {
+			return 0;
+		}
+		return p(0, n, x);
+	};
+};
