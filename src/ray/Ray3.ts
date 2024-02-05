c9400f421ea0fbeacf8b49144781163344fa856f
大聪明 <601670314@qq.com>
2024-02-05 01:33:08 -0800

    Update Ray3.ts

--- src/ray/Ray3.ts
+++ src/ray/Ray3.ts
@@ -2,6 +2,10 @@ import { Vector3, Vector3Like } from "../vector/Vector3";
 import { IRay3 } from "./interfaces/IRay3";
 import { ISphere } from "../shape/interfaces/ISphere";
 import { Plane3 } from "../shape/Plane3";
+import { ITriangle3, Triangle3 } from "../shape";
+import { EPSILON } from "../constants";
+import { Matrix4, Matrix4Like } from "../matrix";
+import { Vector2Like } from "../vector";
 
 let dis: number;
 let r2: number;
@@ -10,10 +14,12 @@ const v = new Vector3();
 
 export class Ray3 implements IRay3 {
 	public static at = <T extends Vector3Like = Vector3>(a: IRay3, b: number, out: T = new Vector3() as T): T => {
-		return Vector3.multiplyScalar(a.direction, b, out);
+		Vector3.multiplyScalar(a.direction, b, out);
+
+		return Vector3.add(a.position, out, out);
 	};
 
-	public static distanceToPlane = (ray: Ray3, plane: Plane3) => {
+	public static distanceToPlane = (ray: IRay3, plane: Plane3) => {
 		const denominator = Vector3.dot(plane.normal, ray.direction);
 
 		if (denominator === 0) {
@@ -47,6 +53,17 @@ export class Ray3 implements IRay3 {
 		return Vector3.distanceToSquared(v, point);
 	};
 
+	public static fromCameraMatrixAndScreenCoord(projectionMatrix: Matrix4Like, worldMatrix: Matrix4Like, screenCoord: Vector2Like, out: Ray3 = new Ray3): Ray3 {
+		Vector3.fromMatrix4Translate(worldMatrix, out.position);
+		Vector3.fromXYZ(screenCoord[0], screenCoord[1], 0.5, out.direction);
+
+		Matrix4.unproject(out.direction, projectionMatrix, worldMatrix, out.direction);
+		Vector3.minus(out.direction, out.position, out.direction);
+		Vector3.normalize(out.direction, out.direction);
+
+		return out;
+	}
+
 	public static lookAt = (a: IRay3, b: Vector3Like, out: Ray3 = new Ray3()): Ray3 => {
 		if (a !== out) {
 			Vector3.fromArray(a.position, 0, out.position);
@@ -57,8 +74,8 @@ export class Ray3 implements IRay3 {
 		return out;
 	};
 
-	public static intersectPlanePoint = <T extends Vector3Like = Vector3>(
-		ray: Ray3,
+	public static intersectPlane3Point = <T extends Vector3Like = Vector3>(
+		ray: IRay3,
 		plane: Plane3,
 		out: T = new Vector3() as T,
 	): T | null => {
@@ -96,11 +113,25 @@ export class Ray3 implements IRay3 {
 		return Ray3.at(ray, t0, out);
 	};
 
-	public static isIntersectSphere = (ray: IRay3, sphere: ISphere): boolean => {
-		return Ray3.distanceSqToPoint(ray, sphere.position) <= sphere.radius * sphere.radius;
-	};
+	public static intersectsTriangle3Point = <T extends Vector3Like = Vector3>(ray: IRay3, triangle: ITriangle3, epsilon = EPSILON, out: T): null | T => {
+		const plane = new Plane3();
+		plane.fromTriangle3(triangle);
 
-	public static intersectsPlane = (ray: Ray3, plane: Plane3): boolean => {
+		const result = Ray3.intersectPlane3Point(ray, plane, out);
+		if (!result) {
+			return null;
+		}		
+
+		const isInTriangle = Triangle3.containsPoint(triangle, epsilon, out);
+
+		if (!isInTriangle) {
+			return null;
+		}
+		
+		return out;
+	}
+
+	public static intersectsPlane3 = (ray: IRay3, plane: Plane3): boolean => {
 		const distToPoint = plane.distanceToPoint(ray.position);
 
 		if (distToPoint === 0) {
@@ -114,6 +145,10 @@ export class Ray3 implements IRay3 {
 		}
 
 		return false;
+	};
+
+	public static intersectsSphere = (ray: IRay3, sphere: ISphere): boolean => {
+		return Ray3.distanceSqToPoint(ray, sphere.position) <= sphere.radius * sphere.radius;
 	};
 
 	public static recast = (ray: IRay3, distance: number, out: Ray3 = new Ray3()): Ray3 => {
