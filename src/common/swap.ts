8afc7786f66795921fa377cfb7cb60def5e3bac3
大聪明 <601670314@qq.com>
2024-07-05 01:15:04 -0700

    Update swap.ts

--- src/common/swap.ts
+++ src/common/swap.ts
@@ -1,4 +1,4 @@
-export const swap = (arr: any[], a: number, b: number) => {
+export const swap = <T>(arr: T[], a: number, b: number): T[] => {
 	const tmp = arr[a];
 	arr[a] = arr[b];
 	arr[b] = tmp;
